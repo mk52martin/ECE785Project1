1a798fd4ab3e2dcec177160eb89088315fd0749a
mkmarti4 <mkmarti4@ncsu.edu>
2023-02-23 00:25:10 -0500

    added callback python function

---
+++ GPIO-In/Python/callback_gpiozero.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from gpiozero import LED, Button
+from time import sleep
+
+out = LED(17)
+button = Button(27)
+
+
+def on_press():
+    out.on()
+    out.off()
+
+while True:
+    button.when_pressed(on_press)
+    sleep(1000)
+
+    
