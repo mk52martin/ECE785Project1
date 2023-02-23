29de50985ea0f74f7a07f7121e5ab32fb13e42e2
mkmarti4 <mkmarti4@ncsu.edu>
2023-02-22 23:01:58 -0500

    make epoll pulse wider

--- GPIO-In/CPP_sysfs/cpp_sysfs_epoll_echo.cpp
+++ GPIO-In/CPP_sysfs/cpp_sysfs_epoll_echo.cpp
@@ -18,7 +18,7 @@ int main(){
     if(high){
       outGPIO.streamWrite(HIGH); //write new value
       int i = 0;
-      while(i < 100000) {
+      while(i < 10000000) {
         i++;
       }
       outGPIO.streamWrite(LOW); //write new value
